8e68694a3674cf369335a91b38857ed92fc358aa
Christian Greissl <greissl@gdi.de>
2023-11-30 09:23:53 +0100

    update

--- ChristianGreissl/Day1.py
+++ ChristianGreissl/Day1.py
@@ -3,3 +3,14 @@
 with open("Day1.txt") as f:
     input_string = f.readlines()
 print(input_string)
+
+
+def partOne():
+    solution = 0
+    return solution
+
+
+
+def partTwo():
+    solution = 0
+    return solution
